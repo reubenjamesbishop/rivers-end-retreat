c6228c404dbda00a61b13a259331a13394b4fe48
reubenjamesbishop <39641783+reubenjamesbishop@users.noreply.github.com>
2021-02-05 13:12:09 +1030

    added gallery intro and title

--- src/components/pages/Gallery.js
+++ src/components/pages/Gallery.js
@@ -24,6 +24,14 @@ import image_18 from '../../images/kitchen.jpg';
 export default function Gallery() {
     return (
         <>
+            <div class="gallery__intro">
+                <div class="gallery__heading">
+                    <h1>Gallery</h1>
+                </div>
+                <p class="gallery__description">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </div>
             <div class="row">
                 <div class="column">
                     <img src={image_1} />
